543f1f93ff3adf69365dd8efefaea19eb0133f19
Viet Hoang <hoangproo2624@gmail.com>
2024-06-04 14:41:34 +0700

    04/06/2024 - Nextjs basic framework

---
+++ src/app/(auth)/(center)/sign-in/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from '@/styles/auth/sign_in.module.css'
+const Sign_in = () => {
+    return (
+        <div className={styles.test}>
+            Sign_in
+        </div>
+    )
+}
+
+export default Sign_in
