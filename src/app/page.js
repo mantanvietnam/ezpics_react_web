b3eb9cdbc9e862b261ae271d51cbd7daee2d855d
Đức Chính <ducchinhnd135@gmail.com>
2024-06-05 11:52:38 +0700

    05/06/2024 - merge ducchinh

--- src/app/page.js
+++ src/app/page.js
@@ -5,16 +5,11 @@ import Slide from "@/components/Slide";
 export default function Home() {
   return (
     <>
-      Home
       <div>
-        <Link href={'/sign-in'}>
-          sign_in
-        </Link>
+        <Link href={"/sign-in"}>sign_in</Link>
       </div>
       <div>
-        <Link href={'/sign-up'}>
-          sign_up
-        </Link>
+        <Link href={"/sign-up"}>sign_up</Link>
       </div>
       <Slide />
     </>
