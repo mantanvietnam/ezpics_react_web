e7ed8b62c6bba259afbd568e3e469342f19947a3
Hieu Pham <hieupham.code@gmail.com>
2024-06-05 16:09:50 +0700

    29/11/2003-home-banner

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import Slide from "@/components/Slide";
+import HomeBanner from '@/components/HomeBanner';
 
 export default function Home() {
   return (
-    <>
-      Home
-      <div>
-        <Link href={'/sign-in'}>
-          sign_in
-        </Link>
-      </div>
-      <div>
-        <Link href={'/sign-up'}>
-          sign_up
-        </Link>
-      </div>
-      <Slide />
-    </>
+    <div className='w-full pt-[20px] px-[50px]'>
+      <HomeBanner />
+    </div>
   );
 }
