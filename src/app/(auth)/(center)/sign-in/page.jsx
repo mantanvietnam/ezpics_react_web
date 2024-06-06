762a989d816ed637fdfe9f75c9c71847eae4c569
Viet Hoang <hoangproo2624@gmail.com>
2024-06-06 22:13:06 +0700

    6/6/2024 giao diện + validate form đăng nhập

--- src/app/(auth)/(center)/sign-in/page.jsx
+++ src/app/(auth)/(center)/sign-in/page.jsx
@@ -1,41 +1,131 @@
 "use client";
-import { Button, Input } from "antd";
-import styles from '@/styles/auth/sign_in.module.css'
-
+import { loginByPhone } from '@/api/auth';
+import styles from '@/styles/auth/sign_in.module.scss';
+import Link from "next/link";
 import { useState } from "react";
+import * as yup from 'yup';
+
+const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+const schema = yup.object().shape({
+    phone: yup.string().matches(phoneRegExp, 'Số điện thoại không hợp lệ').required('Số điện thoại là bắt buộc'),
+    password: yup.string().min(6, 'Mật khẩu phải có ít nhất 6 ký tự').required('Mật khẩu là bắt buộc'),
+});
+const schemaConfirm = yup.object().shape({
+    phoneConfirm: yup.string().matches(phoneRegExp, 'Số điện thoại không hợp lệ').required('Số điện thoại là bắt buộc'),
+});
+
 export default function Login() {
+    const [phone, setPhone] = useState('');
+    const [password, setPassword] = useState('');
+    const [phoneConfirm, setPhoneConfirm] = useState('');
+    const [errors, setErrors] = useState({});
     const [checkForgotPW, setCheck] = useState(false)
+
     function handleCheckForgot() {
         setCheck(true)
     }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const checkValidate = await schema.validate({ phone, password }, { abortEarly: false });
+            if (checkValidate) {
+                setErrors('');
+                try {
+                    const repon = await loginByPhone({
+                        phone: phone,
+                        password: password,
+                        type_device: "web",
+                    });
+
+                } catch (error) {
+                    // if (axios.isAxiosError(error)) {
+                    //     setError(error.response?.data?.message || error.message);
+                    // } else {
+                    //     setError('An unexpected error occurred');
+                    // }
+                }
+            }
+
+        } catch (err) {
+            const errors = err?.inner?.reduce((acc, error) => {
+                acc[error.path] = error.message;
+                return acc;
+            }, {});
+            setErrors(errors);
+        }
+    };
+    const handleConfirm = async (e) => {
+        e.preventDefault();
+        try {
+            const checkValidate = await schemaConfirm.validate({ phoneConfirm }, { abortEarly: false });
+            if (checkValidate) {
+                setErrors('');
+                console.log('Validate thành công');
+                console.log('Giá trị phone:', phoneConfirm);
+            }
+        } catch (err) {
+            const errors = err.inner.reduce((acc, error) => {
+                acc[error.path] = error.message;
+                return acc;
+            }, {});
+            setErrors(errors);
+        }
+    };
     return (
-        <div className={styles["login-bg"]}>
-            <div className={styles["login-custom"]}>
-                <div className={styles["login-main"]}>
-                    <h3>Ezpics - Dùng là thích! 👋</h3>
-                    <p>Mời bạn đăng nhập công cụ thiết kế siêu tốc đầu tiên tại Việt Nam</p>
-                    {
-                        checkForgotPW ?
-                            <>
-                                <p className={styles["phone-number"]}>Số điện thoại Xác thực</p>
-                                <Input size="large" placeholder="Số điện thoại" />
-                            </> :
-                            <>
-                                <p className={styles["phone-number"]}>Số điện thoại</p>
-                                <Input size="large" placeholder="Số điện thoại" />
-                                <div className={styles["pass"]}>
-                                    <p className="password">Mật khẩu</p>
-                                    <p className={styles["forgot-password"]} onClick={handleCheckForgot}>Quên mật khẩu</p>
+        <div className="h-screen w-screen">
+            <div className={styles["login-bg"]}>
+                <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-40">
+                    <div className={styles.padding}>
+                        <div className="w-full min-h-full flex justify-center items-center">
+                            <div className={styles["login-main"]}>
+                                <div className={styles.title}>
+                                    Ezpics - Dùng là thích! 👋
                                 </div>
-                                <Input size="large" className={styles["input-pass"]} placeholder="Mật khẩu" />
-                                <Button type="primary" className={styles["login-account"]}>Đăng nhập</Button>
-                                <p className={styles.or}>hoặc</p>
-                                <Button className={styles["login-gg"]} type="primary" danger>Đăng nhập bằng Google</Button>
-                                <div className="sign-up">
-                                    <p>Bạn chưa có tài khoản ? - <a href="" className={styles["sign-up-button"]}>Đăng kí</a></p>
-                                </div>
-                            </>
-                    }
+                                <p className={styles.description}>
+                                    Mời bạn đăng nhập công cụ thiết kế siêu tốc đầu tiên tại Việt Nam
+                                </p>
+                                {checkForgotPW ? (
+                                    <>
+                                        <p className={styles.label_input}>Số điện thoại xác thực</p>
+                                        <input type="text"
+                                            value={phoneConfirm}
+                                            placeholder="Số điện thoại"
+                                            onChange={(e) => setPhoneConfirm(e.target.value)}
+                                        />
+                                        {errors.phoneConfirm && <p className={styles.error}>{errors.phoneConfirm}</p>}
+                                        <button className={styles.confirm} onClick={handleConfirm}> Xác nhận </button>
+                                    </>
+                                ) : (
+                                    <>
+                                        <p className={styles.label_input}>Số điện thoại</p>
+                                        <input
+                                            type="text"
+                                            placeholder="Số điện thoại"
+                                            value={phone}
+                                            onChange={(e) => setPhone(e.target.value)}
+                                        />
+                                        {errors.phone && <p className={styles.error}>{errors.phone}</p>}
+                                        <div className={styles.box_pass}>
+                                            <p className={styles.label_input}>Mật khẩu</p>
+                                            <p className={styles.forgot_pass} onClick={handleCheckForgot}>Quên mật khẩu ?</p>
+                                        </div>
+                                        <input
+                                            type="password"
+                                            placeholder="Mật khẩu"
+                                            value={password}
+                                            onChange={(e) => setPassword(e.target.value)}
+                                        />
+                                        {errors.password && <p className={styles.error}>{errors.password}</p>}
+                                        <button className={styles.login} onClick={handleSubmit}> Đăng nhập </button>
+                                        <p className={styles.or}>Hoặc</p>
+                                        <button className={styles.register}>Đăng nhập bằng Google</button>
+                                        <p className={styles.option_regis}>Bạn chưa có tài khoản ? - <Link href={'/sign-up'}>Đăng ký</Link></p>
+                                    </>
+                                )}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
