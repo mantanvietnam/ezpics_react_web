e91145baa4d04651d584c03d1d8b48ac7563dc57
Hieu Pham <hieupham.code@gmail.com>
2024-06-07 15:37:49 +0700

    project page

---
+++ src/app/(main)/project/logo/page.jsx
@@ -0,0 +1,10 @@
+import LogoProductSlider from '@/components/Slide/LogoProductSlider'
+import React from 'react'
+
+export default function page() {
+  return (
+    <>
+      <LogoProductSlider />
+    </>
+  )
+}
