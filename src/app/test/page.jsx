ca7c015cb1b5efa08f07210cdfaf5c10650eb65d
quagsulu <quangnvph24106@fpt.edu.vn>
2024-06-11 13:40:05 +0700

    11-6-2024 building transaction Screen

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 import { CHANGE_STATUS_AUTH, CHANGE_VALUE_TOKEN } from '@/redux/slices/authSlice';
-import { checkAvailableLogin, setCookie } from '@/utils';
+import { checkAvailableLogin, getCookie, setCookie } from '@/utils';
 import { useSession } from 'next-auth/react';
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
@@ -18,10 +18,18 @@ const Index = () => {
         }
         // cần xem lại dependencies
     }, [dispatch, session?.accessToken]);
-
+    console.log('isAuthenticated_Cookie',isAuthenticated_Cookie)
+    console.log(dispatch)
+    console.log('session',session)
+    // console.log('session2',session?.accessToken)
+    getCookie
+    console.log('cookie :',getCookie)
+    var cokkie = document.cookie
+    console.log('cokkie',cokkie)
     return (
         <div>
             {isAuthenticated_Cookie ? 'Wellcome Đã đăng nhập : ' : 'Chưa Đăng nhập'}
+
         </div>
     )
 }
