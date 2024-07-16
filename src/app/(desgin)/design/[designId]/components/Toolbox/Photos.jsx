889c01da93e87c54b0999bbf6fd240835364317a
Đức Chính <ducchinhnd135@gmail.com>
2024-07-16 13:50:59 +0700

    add ui design

---
+++ src/app/(desgin)/design/[designId]/components/Toolbox/Photos.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Photo from "../../Icon/Photo";
+
+const Photos = () => {
+  return (
+    <>
+      <div
+        style={{
+          position: "absolute",
+          top: "0",
+          left: "100px",
+          height: "100%",
+          width: "300px",
+          paddingTop: "94px",
+          borderRight: "1px solid #ccc",
+        }}>
+        Test photo
+      </div>
+    </>
+  );
+};
+
+export default Photos;
