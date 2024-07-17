24c017811a34e4d18137009aa73feb0da5072a30
manh1111 <minhanh746347@gmail.com>
2024-07-17 15:26:31 +0700

    add custommize

--- src/app/(desgin)/design/[designId]/page.jsx
+++ src/app/(desgin)/design/[designId]/page.jsx
@@ -6,9 +6,9 @@ import Toolbox from "./components/Toolbox/Toolbox";
 const page = () => {
   return (
     <>
-      <div style={{ height: "100vh" }}>
-        <Navbar />
-        <Toolbox />
+      <Navbar />
+      <div style={{ height: "100vh", padding: "65px 0px 0px 0px"}}>
+        <Toolbox  />
         <div className="relative z-1 bg-gray-300 h-[100%] ml-[396px]">
           <div className="flex h-[100%] justify-center items-center">
             <div className="h-[200px] w-[400px] bg-red-300"></div>
