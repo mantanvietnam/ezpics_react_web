a57ea5d9af959ed4605e32473a1343de18c0711a
Đức Chính <ducchinhnd135@gmail.com>
2024-07-19 11:07:04 +0700

    add thanh panels

--- src/app/(desgin)/design/[designId]/page.jsx
+++ src/app/(desgin)/design/[designId]/page.jsx
@@ -1,25 +1,26 @@
-"use client"
-import { useEffect, useState } from "react"
-import Navbar from "./components/Navbar/Navbar"
-import Toolbox from "./components/Toolbox/Toolbox"
-import { useParams } from "next/navigation"
-import { getListLayerApi } from "../../../../api/design"
-import { checkTokenCookie } from "@/utils"
-import { Stage, Layer, Rect, Image } from "react-konva"
-import BackgroundLayer from "./components/Editor/BackgroundLayer"
-import ImageLayer from "./components/Editor/ImageLayer"
-import TextLayer from "./components/Editor/TextLayer"
+"use client";
+import { useEffect, useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Toolbox from "./components/Toolbox/Toolbox";
+import { useParams } from "next/navigation";
+import { getListLayerApi } from "../../../../api/design";
+import { checkTokenCookie } from "@/utils";
+import { Stage, Layer, Rect, Image } from "react-konva";
+import BackgroundLayer from "./components/Editor/BackgroundLayer";
+import ImageLayer from "./components/Editor/ImageLayer";
+import TextLayer from "./components/Editor/TextLayer";
+import PanelsImage from "./components/Panels/PanelsImage";
 
 const Page = () => {
-  const params = useParams()
-  const { designId } = params
+  const params = useParams();
+  const { designId } = params;
 
-  const [design, setDesign] = useState()
-  const [designLayers, setDesignLayers] = useState([])
-  const [initSize, setInitSize] = useState({ width: 0, height: 0 })
-  const [selectedId, setSelectedId] = useState(null)
+  const [design, setDesign] = useState();
+  const [designLayers, setDesignLayers] = useState([]);
+  const [initSize, setInitSize] = useState({ width: 0, height: 0 });
+  const [selectedId, setSelectedId] = useState(null);
 
-  const [activeTool, setActiveTool] = useState("Layer")
+  const [activeTool, setActiveTool] = useState("Layer");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,30 +28,39 @@ const Page = () => {
         const response = await getListLayerApi({
           idproduct: designId,
           token: checkTokenCookie(),
-        })
+        });
         if (response.code === 1) {
-          setDesign(response.data)
-          setDesignLayers(response.data.productDetail)
+          setDesign(response.data);
+          setDesignLayers(response.data.productDetail);
           if (response.data.width >= 4000 || response.data.height >= 4000) {
             setInitSize({
               width: response.data.width / 7,
               height: response.data.height / 7,
-            })
-          } else if (response.data.width >= 4000 || response.data.height >= 4000) {
+            });
+          } else if (
+            response.data.width >= 4000 ||
+            response.data.height >= 4000
+          ) {
             setInitSize({
               width: response.data.width / 6,
               height: response.data.height / 6,
-            })
-          } else if (response.data.width >= 3000 || response.data.height >= 3000) {
+            });
+          } else if (
+            response.data.width >= 3000 ||
+            response.data.height >= 3000
+          ) {
             setInitSize({
               width: response.data.width / 5,
               height: response.data.height / 5,
-            })
-          } else if (response.data.width >= 2500 || response.data.height >= 2500) {
+            });
+          } else if (
+            response.data.width >= 2500 ||
+            response.data.height >= 2500
+          ) {
             setInitSize({
               width: response.data.width / 3,
               height: response.data.height / 3,
-            })
+            });
           } else if (
             response.data.width >= 1920 ||
             response.data.height >= 1920
@@ -58,7 +68,7 @@ const Page = () => {
             setInitSize({
               width: response.data.width / 2.5,
               height: response.data.height / 2.5,
-            })
+            });
           } else if (
             response.data.width >= 1600 ||
             response.data.height >= 1600
@@ -66,7 +76,7 @@ const Page = () => {
             setInitSize({
               width: response.data.width / 1.5,
               height: response.data.height / 1.5,
-            })
+            });
           } else if (
             response.data.width >= 1000 ||
             response.data.height >= 1000
@@ -74,7 +84,7 @@ const Page = () => {
             setInitSize({
               width: response.data.width / 1.5,
               height: response.data.height / 1.5,
-            })
+            });
           } else if (
             response.data.width >= 500 ||
             response.data.height >= 500
@@ -82,21 +92,20 @@ const Page = () => {
             setInitSize({
               width: response.data.width * 1.5,
               height: response.data.height * 1.5,
-            })
+            });
           } else {
             setInitSize({
               width: response.data.width * 2,
               height: response.data.height * 2,
-            })
+            });
           }
         }
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-    }
-    fetchData()
-  }, [designId])
-
+    };
+    fetchData();
+  }, [designId]);
 
   const checkDeselect = (e) => {
     // deselect when clicked on empty area
@@ -104,10 +113,10 @@ const Page = () => {
     if (clickedOnEmpty) {
       setSelectedId(null);
     }
-  }
+  };
 
-  console.log("🚀 ~ Page ~ design:", design)
-  console.log('🚀 ~ Page ~ selectedId:', selectedId)
+  console.log("🚀 ~ Page ~ design:", design);
+  console.log("🚀 ~ Page ~ selectedId:", selectedId);
 
   return (
     <>
@@ -118,17 +127,18 @@ const Page = () => {
           padding: "64px 0px 0px 0px",
         }}>
         <Toolbox onToolChange={setActiveTool} />
+        <PanelsImage />
         <div
-          className={`relative z-1 bg-gray-300 h-[100%] transition-all duration-300 ${activeTool ? "ml-[396px]" : "ml-[96px]"
-            }`}>
+          className={`relative z-1 bg-gray-300 h-[calc(100%-50px)] transition-all duration-300 ${
+            activeTool ? "ml-[396px]" : "ml-[96px]"
+          }`}>
           <div className="flex h-[100%] justify-center items-center">
             <Stage
               width={initSize.width}
               height={initSize.height}
               className="bg-white"
               onMouseDown={checkDeselect}
-              onTouchStart={checkDeselect}
-            >
+              onTouchStart={checkDeselect}>
               <Layer>
                 <BackgroundLayer
                   src={design?.thumn}
@@ -154,7 +164,7 @@ const Page = () => {
                         }}
                       />
                     </Layer>
-                  )
+                  );
                 } else if (layer.content.type === "text") {
                   return (
                     <Layer key={layer.id}>
@@ -171,7 +181,7 @@ const Page = () => {
                         }}
                       />
                     </Layer>
-                  )
+                  );
                 }
               })}
             </Stage>
@@ -179,7 +189,7 @@ const Page = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
