16162f09e84d7b4ea93886890d5dd48fdadbbd01
Đức Chính <ducchinhnd135@gmail.com>
2024-07-19 15:55:51 +0700

    add image

--- src/app/(desgin)/design/[designId]/components/Toolbox/Photos.jsx
+++ src/app/(desgin)/design/[designId]/components/Toolbox/Photos.jsx
@@ -2,9 +2,14 @@
 import React, { useEffect, useState } from "react";
 import { checkTokenCookie } from "@/utils";
 import axios from "axios";
+import { useParams } from "next/navigation";
 
 const Photos = () => {
   const [photos, setPhotos] = useState([]);
+  const inputFileRef = React.useRef(null);
+
+  const params = useParams();
+  const { designId } = params;
 
   useEffect(() => {
     async function fetchData() {
@@ -24,14 +29,59 @@ const Photos = () => {
     fetchData();
   }, []);
 
+  const handleDropFiles = async (files) => {
+    const file = files[0];
+    const url = URL.createObjectURL(file);
+    if (!/(png|jpg|jpeg)$/i.test(file.name)) {
+      toast.error("Chỉ chấp nhận file png, jpg hoặc jpeg");
+      return;
+    }
+
+    const res = await axios.post(
+      `https://apis.ezpics.vn/apis/addLayerImageAPI`,
+      {
+        idproduct: designId,
+        token: checkTokenCookie(),
+        file: file,
+        page: 0,
+      },
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+
+    if (res.data.code === 1) {
+      console.log(res.data);
+    }
+  };
+
+  const handleInputFileRefClick = () => {
+    inputFileRef.current?.click();
+  };
+
+  const handleFileInput = (e) => {
+    handleDropFiles(e.target.files);
+  };
+
   return (
     <>
       <div className="absolute top-0 left-[100px] h-full w-[300px] overflow-y-scroll">
         <div className="px-4 py-4">
           <h4 className="py-2">Ảnh tải lên</h4>
-          <button className="w-[100%] bg-black rounded-lg border border-transparent text-white px-4 py-2 hover:bg-white hover:text-black hover:border-black transition-colors duration-300">
+          <button
+            className="w-[100%] bg-black rounded-lg border border-transparent text-white px-4 py-2 hover:bg-white hover:text-black hover:border-black transition-colors duration-300"
+            onClick={handleInputFileRefClick}>
             Chọn ảnh từ máy
           </button>
+          <input
+            onChange={handleFileInput}
+            type="file"
+            id="file"
+            ref={inputFileRef}
+            style={{ display: "none" }}
+          />
         </div>
         <div className="px-4">
           <h4>Ảnh đã tải lên</h4>
