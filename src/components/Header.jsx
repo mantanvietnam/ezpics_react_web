c7672df1f54151b51ad5bc05845b8b018b812d8a
Tran Duc Chinh <ducchinhnd135@gmail.com>
2024-08-04 15:06:25 +0700

    fix lỗi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -379,13 +379,12 @@ const Header = ({ toggleNavbar }) => {
     const response = await logoutService({
       token: checkTokenCookie(),
     });
+    // await signOut({});
     clearAllCookies();
-    await signOut({});
     document.cookie = `user_login=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
     document.cookie = `token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
     dispatch(DELETE_ALL_VALUES());
 
-    clearAllCookies();
     router.push("https://ezpics.vn");
     // }
   };
